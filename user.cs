ab786a4f88f1b0dd843335733040e7ca512cc82f
Miguel Angel Garzon <hellomiguel1986@gmail.com>
2025-06-18 17:42:43 -0500

    Complete classes

---
+++ user.cs
@@ -0,0 +1,8 @@
+public class user
+{
+    public int Id { get; set; }
+    public string Fristname { get; set; }
+    public string Lastname { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
